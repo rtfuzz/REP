a1b9b9bf86b00eba2bc8b20187b668e229fa4aca
rtfuzz <rtfuzz@gmail.com>
2016-07-01 22:13:14 -0300

    app
    app

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://www.apetitego.com.br/mobileapp/api",
+	'DialogDefaultTitle' : "Seu apetite bem cuidado.",
+	'pushNotificationSenderid' : "172997422108",
+	'facebookAppId' : "213633375702109",
+	'APIHasKey' : "Fuzz0f869170c3e89a9c2719ef5TheStarb080d"
+};
